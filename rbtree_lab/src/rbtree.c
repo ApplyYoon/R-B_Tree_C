626493e3e7e45d9d5e39f9d954fa5e9425940d74
apply <wonji052271@gmail.com>
2025-10-19 23:45:32 +0900

    case1 error exist

--- rbtree_lab/src/rbtree.c
+++ rbtree_lab/src/rbtree.c
@@ -1,5 +1,5 @@
 #include "rbtree.h"
-
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -7,11 +7,15 @@ rbtree *new_rbtree(void) {
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
   node_t *NIL = (node_t *)calloc(1, sizeof(node_t));
   
+  // NIL self
   NIL->color = RBTREE_BLACK;
+  NIL->left = NIL;
+  NIL->right = NIL;
+  NIL->parent = NIL;
 
   p->nil = NIL;
   p->root = NIL;  
-  p->root->parent = NULL;
+  // p->root->parent = NULL;
 
   printf("( !! New Tree !! )\n");
   return p;
@@ -22,6 +26,10 @@ void delete_rbtree(rbtree *t) {
   free(t);
 }
 
+char getColorName(color_t nodeColor) {
+  if (nodeColor == 0) return 'R';
+  else return 'B';
+}
 int case3(rbtree *t) {
 
   return 0;
@@ -31,35 +39,81 @@ int case2(rbtree *t) {
   return 0;
 }
 
-int case1(rbtree *t) {
-  node_t *cur = t->root;
+void case1(rbtree *t, node_t *cur, char parentDirect) {
+  node_t *NIL = t->nil;
 
-  return 0;
+  bool condition = ((cur != NIL && cur->parent != NIL && cur->parent->parent != NIL));
+
+  printf("condition %d\n", condition);
+
+  if (!condition) return;
+  if (parentDirect == 'L' && cur->parent->parent->left == NIL) return;
+  if (parentDirect == 'R' && cur->parent->parent->right == NIL) return;
+
+  // change condition
+  printf("parent: %c\n", parentDirect);
+
+  if (parentDirect == 'R') {
+    condition = (((cur->color == RBTREE_RED) && (cur->parent->color == RBTREE_RED)) && cur->parent->parent->left->color == RBTREE_BLACK);
+  }
+  else if (parentDirect == 'L') {
+    condition = (((cur->color == RBTREE_RED) && (cur->parent->color == RBTREE_RED)) && cur->parent->parent->right->color == RBTREE_BLACK);
+  }
+
+  if (!condition) return;
+
+
+  // for debuging  
+  printf("---------------[ INFO ]--------------\n");
+  printf("cur -> key: %d, color: %c\n", cur->key, getColorName(cur->color));
+  printf("WELCOME TO CASE1\n");
+
+  cur->color = RBTREE_BLACK;
+  cur->parent->color = RBTREE_BLACK;
+
+  if (parentDirect == 'L') {
+    cur->parent->right = RBTREE_RED;
+  }
+  // else {    // if curDirect == 'R'
+  //   cur->parent->left = RBTREE_RED;
+  // }
+  return;
 }
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
   // TODO: implement insert
-  printf("!\n");
   node_t *cur = t->root; 
-  printf("!\n");
   node_t *NIL = t->nil;
-  printf("!\n");
-  rbtree *p = (node_t *)calloc(1, sizeof(node_t));
+
+  // Set new node
+  node_t *p = (node_t *)calloc(1, sizeof(node_t));
+  char parentDiret;
+
+  p->key = key;
+  p->color = RBTREE_RED;
+  p->left = NIL;
+  p->right = NIL;
+
+  // root is black
+  t->root->color = RBTREE_BLACK;
 
   printf("[insert] root: %d\n", t->root->key);
   printf("[insert] cur: %d\n", cur->key);
   printf("[insert] key: %d\n", key);
+  
   // printf("NIL: %d\n", NIL->key);
 
   // root
-  if (t->root->key == NULL) {
-    t->root->key = key;
-    t->nil = NULL;
+  if (t->root == NIL) {
+    t->root = p;
+    t->root->left = NIL;
+    t->root->right = NIL;
+    t->root->parent = NIL;
     t->root->color = RBTREE_BLACK;
     return t->root;
   }
   else {
-    while (cur != NULL)
+    while (cur != NIL)
     {
       // need to the same-number case
 
@@ -68,35 +122,50 @@ node_t *rbtree_insert(rbtree *t, const key_t key) {
         if (cur->left != NIL) {
           printf("[inesrt] Move to left\n");
           cur = cur->left;
+          parentDiret = 'L';
         }
-        else {
+        else if (cur->left == NIL) {
           printf("[insert](left) key: %d \n", cur->key);
+
+          p->parent = cur;  // link parent
           cur->left = p;
-          cur->left->key = key;
-          cur->left->color = RBTREE_RED;
-          
-          cur->left->left = NIL;
-          cur->left->right = NIL;
-          return cur->left;
+    
+          printf("[insert](new) key: %d \n", cur->right->key);
+
+
+          // move to new node
+          cur = cur->left;
+
+          // case 1
+          case1(t, cur, parentDiret);
+
+          return cur; 
         }
       }
-
       // the key is in the right subtree
       else if (key > cur->key) {
         if (cur->right != NIL) {
           printf("[inesrt] Move to right\n");
           cur = cur->right;
+          parentDiret = 'R';
         }
-        else {
+        else if (cur->right == NIL) {
           printf("[insert](right) key: %d \n", cur->key);
+
+          p->parent = cur;  // link parent
           cur->right = p;
-          cur->right->key = key;
-          cur->right->color = RBTREE_RED;
-          
-          cur->right->left = NIL;
-          cur->right->right = NIL;
+
           printf("[insert](new) key: %d \n", cur->right->key);
-          return cur->right;
+
+          // move to new node
+          cur = cur->right;
+          
+
+          // case 1
+          case1(t, cur, parentDiret);
+          
+          
+          return cur;
         }
       }
     }
